d93fabad2f7b234e0f7b2bdd09886b5d84caf30d
Tamura <tamurafelipe@gmail.com>
2022-06-22 13:21:00 -0300

    Commit RocketNFTs

---
+++ script.js
@@ -0,0 +1,14 @@
+ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 700
+}).reveal(`
+    .home, 
+    .main-one,
+    .gallery,
+    .main-two,
+    .main-three,
+    .main-four,
+    footer,
+    img`)
+
